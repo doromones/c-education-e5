205993e68ea977e64ec5d5218467b4ba3466b96b
Anton Radushev <a.radushev@ticketstravelnetwork.net>
2016-07-27 11:03:50 +0300

    init

---
+++ js/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var images = [
+    'https://goo.gl/qj0ksu',
+    'https://goo.gl/Uz5j6c',
+    'https://goo.gl/7F1Fx2',
+    'https://vermilion1.github.io/presentations/grunt/images/grunt-logo.png',
+    'https://goo.gl/L11FEv',
+    'https://goo.gl/YkorhO'
+];
+
+window.addEventListener('load', function(){
+    alert('qweqwe')
+})
